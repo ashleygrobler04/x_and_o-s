7690514299483e4874dfe78678a7786c97572fa7
ashleygrobler04 <ashleygrobler04@gmail.com>
2022-01-16 09:05:43 +0200

    tried to detect screen readers.

--- speech.py
+++ speech.py
@@ -1,5 +1,8 @@
 from cytolk import tolk
 
 def speak(text):
+    tolk.try_sapi(True)
+    tolk.prefer_sapi(False)
     with tolk.tolk():
+        tolk.detect_screen_reader()
         tolk.speak(text)
