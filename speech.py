34ee1bd34a6baa725b7b19fe409ae6fc5212ee71
ashleygrobler04 <ashleygrobler04@gmail.com>
2022-01-16 09:50:59 +0200

    tried to fix tts again but doesn't work

--- speech.py
+++ speech.py
@@ -1,8 +1,11 @@
 from cytolk import tolk
 
+
+tolk.try_sapi(True)
+tolk.prefer_sapi(False)
+with tolk.tolk():
+    tolk.detect_screen_reader()
+    speak(text)
+
 def speak(text):
-    tolk.try_sapi(True)
-    tolk.prefer_sapi(False)
-    with tolk.tolk():
-        tolk.detect_screen_reader()
-        tolk.speak(text)
+    tolk.speak(text)
